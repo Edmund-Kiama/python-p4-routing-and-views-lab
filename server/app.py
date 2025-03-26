5f4fcfee7e1517b6020cebe177da3d83e25c082d
Edmund <edmundgachanja@gmail.com>
2025-03-26 16:19:46 +0300

    chore: completes lab

--- server/app.py
+++ server/app.py
@@ -10,14 +10,19 @@ def index():
 
 @app.route('/print/<string:parameter>')
 def print_string(parameter):
-    print("parameter")
-    return f'{parameter}'
+    print(parameter)
+    return parameter
 
 @app.route('/count/<int:integer>')
 def count(integer):
-    return f'{[number for number in range(integer)]}'
+    count_list = []
 
-@app.route('/math/<int:num1>/<operation>/<int:num2>')
+    for number in range(integer):
+        count_list.append(str(number))
+    
+    return "\n".join(count_list) + "\n"
+
+@app.route('/math/<int:num1>/<string:operation>/<int:num2>')
 def math(num1, operation, num2):
     op = False
     if operation == "+":
@@ -28,7 +33,9 @@ def math(num1, operation, num2):
         op = num1 / num2
     elif operation == "*":
         op = num1 * num2
-    return f'{op}'
+    elif operation == '%':
+        op = num1 % num2
+    return str(op)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
